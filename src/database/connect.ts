7ccdd7f598ca1707ee7f9e076bb1261e3fe8f1e3
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-03-02 03:02:36 +0800

    Dotenv configurations

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,10 +1,8 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const Connect = () => {
-  const uri =
-    "mongodb+srv://jamesnative:6KEaM2icAY0it5s1@native-shoe-db.wbaghbd.mongodb.net/?retryWrites=true&w=majority";
   mongoose
-    .connect(uri)
+    .connect(`${process.env.MONGO_DB}`)
     .then(() => {
       console.log("DATABASE SUCCESS");
     })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Application } from "express";
-
+import dotenv from "dotenv";
 import commentRouter from "./routes/comments/index";
 import Connect from "./database/connect";
+
+dotenv.config();
 const app: Application = express();
 app.use(express.json());
 app.use("/products", commentRouter);
